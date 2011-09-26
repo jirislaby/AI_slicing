a1fe24cf1d9a6867ca825cf4836aeab12bcfaea7
Jiri Slaby <jirislaby@gmail.com>
2011-09-26 19:41:45 +0200

    llvm-builder: build with debug info
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- llvm-builder.sh
+++ llvm-builder.sh
@@ -6,5 +6,5 @@ if [ $# -gt 0 ]; then
 fi
 
 find "$DIR" -type f -name '*.i' -print \
-	     -exec clang -emit-llvm -c {} -o {}.llvm -O0 -w \; \
+	     -exec clang -g -emit-llvm -c {} -o {}.llvm -O0 -w \; \
 	     -exec opt -load LLVMSlicer.so -slice {}.llvm -o {}.llvm.sliced \;
