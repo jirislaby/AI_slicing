41c9f529fc78f1af8d01bc96cba0db3bc18a57ff
Jiri Slaby <jirislaby@gmail.com>
2011-09-30 10:31:39 +0200

    llvm-builder: simplify CFG after slicing
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- llvm-builder.sh
+++ llvm-builder.sh
@@ -7,4 +7,4 @@ fi
 
 find "$DIR" -type f -name '*.i' -print \
 	     -exec clang -g -emit-llvm -c {} -o {}.llvm -O0 -w \; \
-	     -exec opt -load LLVMSlicer.so -slice {}.llvm -o {}.llvm.sliced \;
+	     -exec opt -load LLVMSlicer.so -slice {}.llvm -simplifycfg -o {}.llvm.sliced \;
