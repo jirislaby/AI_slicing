b27d34c7365c8da948405ecde9e60b1d5957371b
Jiri Slaby <jirislaby@gmail.com>
2011-11-30 13:26:50 +0100

    library: add a basic one
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

---
+++ library.c
@@ -0,0 +1,12 @@
+int param_array_get() {return 0;}
+void param_array_set() {}
+int param_get_int() {return 0;}
+short param_get_short() {return 0;}
+void param_set_int() {}
+void param_set_short() {}
+
+unsigned long simple_strtoul(const char *cp, char **endp, unsigned int base)
+{
+	return 0;
+}
+
