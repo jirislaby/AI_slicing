34bcbe4f169c75b89b9f1451752b5b38cba804a3
Jiri Slaby <jirislaby@gmail.com>
2011-11-30 13:33:28 +0100

    llvm-builder: speed up
    Parallelize and check what's really needed to be rebuilt.
    
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- llvm-builder.sh
+++ llvm-builder.sh
@@ -5,6 +5,7 @@ if [ $# -gt 0 ]; then
 	DIR=$1
 fi
 
-find "$DIR" -type f -name '*.i' -print \
-	     -exec clang -g -emit-llvm -c {} -o {}.llvm -O0 -w \; \
-	     -exec opt -load LLVMSlicer.so -slice {}.llvm -simplifycfg -o {}.llvm.sliced \;
+find "$DIR" -type f -name '*.i' -print0 | xargs -t -P3 -0 -n1 -I{} \
+	sh -c 'test ! -f {}.llvm -o {}.llvm -ot {} && clang -g -emit-llvm -c {} -o {}.llvm -O0 -w; true'
+
+#	     -exec opt -load LLVMSlicer.so -slice {}.llvm -simplifycfg -o {}.llvm.sliced \;
