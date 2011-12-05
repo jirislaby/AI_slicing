9e328adbd6d6fc8161803fd8116ee8cb92a5d75b
Jiri Slaby <jirislaby@gmail.com>
2011-12-05 16:19:14 +0100

    i-builder: remove some commented stuff
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- i-builder.sh
+++ i-builder.sh
@@ -9,12 +9,7 @@ cat >$BLD_SCRIPT <<EOF
 #!/bin/bash
 
 echo invoked \${#@}
-#MAK="make -k $MAKE_OPT"
 MAK="make -k $MAKE_OPT \`echo \$@|sed 's/\.c\>/.i/g'\`"
-#echo \$@|sed 's/\.c\>/.i/g'
-#for file in \$@; do
-#	MAK="\$MAK \${file%.c}.i"
-#done
 \$MAK
 EOF
 
