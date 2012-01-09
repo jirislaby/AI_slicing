95b4b8ebca05a304b17a00aea88ccfffa406fc17
Jiri Slaby <jirislaby@gmail.com>
2012-01-09 15:34:25 +0100

    llvm-builder: prepare and link the library
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- llvm-builder.sh
+++ llvm-builder.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+if [ -z "$LIB" ]; then
+	LIB=library.c
+fi
+if [ ! -f "$LIB" ]; then
+	echo "Cannot find library '$LIB'" >&2
+	exit 1
+fi
+
 DIR=.
 if [ $# -gt 0 ]; then
 	DIR=$1
 fi
 
-find "$DIR" -type f -name '*.i' -print0 | xargs -t -P3 -0 -n1 -I{} \
-	sh -c 'test ! -f {}.llvm -o {}.llvm -ot {} && clang -g -emit-llvm -c {} -o {}.llvm -O0 -w; true'
+clang -g -nostdinc -emit-llvm -c "$LIB" -o library.o -O2 || exit 1
+
+find "$DIR" -type f -name '*.i' -print0| xargs -t -P3 -0 -n1 -I{} \
+	sh -c 'test ! -f {}.llvm -o {}.llvm -ot {} &&
+		clang -g -nostdinc -emit-llvm -O0 -w -c {} -o {}.comp 2>/dev/null &&
+		opt -load LLVMSlicer.so -prepare {}.comp -o {}.prep &&
+		llvm-link -o {}.llvm {}.prep library.o &&
+		rm -f {}.comp {}.prep 2>/dev/null;
+		true'
 
 #	     -exec opt -load LLVMSlicer.so -slice {}.llvm -simplifycfg -o {}.llvm.sliced \;
