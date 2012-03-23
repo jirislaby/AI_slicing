51710dd84e9dec988d5b43aea0c788b6868ff998
Jiri Slaby <jirislaby@gmail.com>
2012-03-23 23:39:06 +0100

    line_to_fun: now working
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- line_to_fun.pl
+++ line_to_fun.pl
@@ -3,24 +3,45 @@ use strict;
 
 my $jf = $ARGV[0];
 shift;
-my $chop = $ARGV[0];
+my $crop = $ARGV[0];
+my $croplen = length($crop);
 shift;
 
+my %funs;
 my %flags;
 
 open(JF, "<$jf") || die "cannot open '$jf'";
 while (<JF>) {
 	/^{(.*)},{(.*)},{(.*)},{(.*)}$/ || die "bad input";
 	my $file = $1;
-
-	$flags{$1} = [ $1, $2, $3, $4 ];
+	if (substr($file, 0, $croplen) eq $crop) {
+		$file = substr($file, $croplen);
+	}
+	$flags{$file} = [ $1, $2, $3, $4 ];
 }
 close JF;
 
 while (<>) {
 	chomp;
 	my ($file, $line) = split / /;
-	print "$file XX $line\n";
+	my $arrref = $flags{$file};
+	die "not found" unless (defined $arrref);
+	my $flags = "gcc -E -w $$arrref[3] $$arrref[0] | ".
+		"clang -cc1 -w -analyze -analyzer-checker experimental.core.FunLines|";
+	open(FUNS, $flags) || die "cannot exec clang";
+	while (<FUNS>) {
+		chomp;
+		/^(.+): (.+):([0-9]+)-([0-9]+)$/ || die "bad input";
+		my $file1 = $2;
+		if (substr($file1, 0, $croplen) eq $crop) {
+			$file1 = substr($file1, $croplen);
+		}
+		if ($file eq $file1 && $3 <= $line && $line <= $4) {
+			print "$file:$line converted to $1\n"
+		}
+#		print "$file1: $3-$4 $1\n";
+	}
+	close FUNS;
 }
 
 1;
