86d64621d3aa0788327e29f1baeec6fdd2aa1b6c
Jiri Slaby <jirislaby@gmail.com>
2012-07-12 10:21:20 +0200

    llvm-builder: don't build lib with optimizations
    It confuses klee which in turn reports invalid results. For example
    when analysing sound/oss/sscape.c it does not report a memory error.

--- llvm-builder.sh
+++ llvm-builder.sh
@@ -13,7 +13,7 @@ if [ $# -gt 0 ]; then
 	DIR=$1
 fi
 
-clang -g -nostdinc -emit-llvm -c "$LIB" -o library.o -O2 || exit 1
+clang -g -nostdinc -emit-llvm -c "$LIB" -o library.o -O0 || exit 1
 
 find "$DIR" -type f -name '*.i' -print0| xargs -t -P3 -0 -n1 -I{} \
 	sh -c 'test ! -f {}.llvm -o {}.llvm -ot {} &&
