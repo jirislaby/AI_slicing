4312d029ce0696f1dc6ac1385e2f870b10796a5e
Jiri Slaby <jslaby@suse.cz>
2013-02-19 14:50:48 +0100

    get_starting_funs: better output

--- get_starting_funs.sh
+++ get_starting_funs.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 while read FILE FUN; do
-	if [ -f $FILE.main.$FUN.o ]; then
-		echo $FILE.main.$FUN.o
+	test -f "$FILE" || continue
+	echo "A $FILE $FUN"
+	if [ -f "$FILE.main.$FUN.o" ]; then
+		echo B $FILE.main.$FUN.o
 		continue
 	fi
 	for fun in `grep '^CALLREL .* => '"$FUN"'$' $FILE.callrel | \
 			sed 's/^CALLREL \(.*\) => .*$/\1/'`; do
 		if [ -f $FILE.main.$fun.o ]; then
-			echo $FILE.main.$fun.o
+			echo B $FILE.main.$fun.o
 		else
-			echo $FILE.main.$fun.o not found >&2
+#			echo $FILE.main.$fun.o not found >&2
+:
 		fi
 	done
 done
