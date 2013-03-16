20dc348a615f9d2444bfb3b8feb6755ee16b5cc6
Jiri Slaby <jslaby@suse.cz>
2013-03-16 22:23:07 +0100

    klee-log-parser: support more errors

--- klee-log-parser.sh
+++ klee-log-parser.sh
@@ -7,11 +7,14 @@ else
 fi
 
 for file in $FILES; do
+#	grep -q 'LastChar > FirstChar' $file && echo -n X
 	grep -q 'query timed out (resolve)' $file && echo -n H
 	grep -q 'HaltTimer invoked' $file && echo -n H
 	grep -q 'failed external call' $file && echo -n E
+	grep -q 'ERROR: unable to load symbol' $file && echo -n G
 	grep -qE 'memory error|invalid function pointer' $file && echo -n M
 	grep -q 'LLVM ERROR: Code generator does not support' $file && echo -n I
 	grep -q 'klee: .*Assertion .* failed.' $file && echo -n C
-	grep -q 'ASSERTION FAIL' $file && echo -n B; echo " $file"
+	grep -q 'ASSERTION FAIL' $file && echo -n B
+	echo " $file"
 done
