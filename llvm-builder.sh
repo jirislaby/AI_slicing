90f861eb2a16e2cc1e80905b30a92775fc405bef
Jiri Slaby <jirislaby@gmail.com>
2013-09-12 19:52:12 +0200

    llvm-builder: run kleerer too

--- llvm-builder.sh
+++ llvm-builder.sh
@@ -17,12 +17,13 @@ if [ library.o -ot "$LIB" ]; then
 	clang -g -nostdinc -emit-llvm -c "$LIB" -o library.o -O0 || exit 1
 fi
 
-find "$DIR" -type f -name '*.i' -print0| xargs -t -P3 -0 -n1 -I{} \
+find "$DIR" -type f -name '*.i' -print0| xargs -t -P4 -0 -n1 -I{} \
 	sh -c 'test ! -f {}.llvm -o {}.llvm -ot {} -o {}.llvm -ot library.o &&
 		clang -g -nostdinc -emit-llvm -O0 -w -c {} -o {}.comp &&
 		opt -load LLVMSlicer.so -prepare {}.comp -o {}.prep &&
 		llvm-link -o {}.llvm {}.prep library.o &&
+		opt -load LLVMSlicer.so -kleerer -disable-output {}.llvm &&
 		rm -f {}.comp {}.prep 2>/dev/null;
 		true'
 
-#	     -exec opt -load LLVMSlicer.so -slice {}.llvm -simplifycfg -o {}.llvm.sliced \;
+#		opt -load LLVMSlicer.so -simplifycfg -create-hammock-cfg -slice-inter -simplifycfg {}.llvm -o {}.llvm.sliced &&
