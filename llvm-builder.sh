eb49b3e1432692bb8fed9eeae7a8cdb09dd636cc
Jiri Slaby <jirislaby@gmail.com>
2018-10-03 08:19:01 +0200

    more updates
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- llvm-builder.sh
+++ llvm-builder.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -xe
+
 if [ -z "$LIB" ]; then
 	LIB=library.c
 fi
@@ -13,17 +15,19 @@ if [ $# -gt 0 ]; then
 	DIR=$1
 fi
 
+OPTNONE="-O0 -Xclang -disable-O0-optnone"
+
 if [ library.o -ot "$LIB" ]; then
-	clang -g -nostdinc -emit-llvm -c "$LIB" -o library.o -O0 || exit 1
+	clang -g -nostdinc -emit-llvm -c "$LIB" -o library.o $OPTNONE || exit 1
 fi
 
 find "$DIR" -type f -name '*.i' -print0| xargs -t -P4 -0 -n1 -I{} \
-	sh -c 'test ! -f {}.llvm -o {}.llvm -ot {} -o {}.llvm -ot library.o &&
-		clang -g -nostdinc -emit-llvm -O0 -w -c {} -o {}.comp &&
+	sh -c "test ! -f {}.llvm -o {}.llvm -ot {} -o {}.llvm -ot library.o &&
+		clang -g -nostdinc -emit-llvm $OPTNONE -w -c {} -o {}.comp &&
 		opt -load LLVMSlicer.so -prepare {}.comp -o {}.prep &&
 		llvm-link -o {}.llvm {}.prep library.o &&
 		opt -load LLVMSlicer.so -kleerer -disable-output {}.llvm &&
 		rm -f {}.comp {}.prep 2>/dev/null;
-		true'
+		true; false"
 
 #		opt -load LLVMSlicer.so -simplifycfg -create-hammock-cfg -slice-inter -simplifycfg {}.llvm -o {}.llvm.sliced &&
